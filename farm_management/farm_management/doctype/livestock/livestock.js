a266ac769850d6cfd7810e29df8c1fcc6456e649
MohamDah <mohammedfaiseldhb@gmail.com>
2025-04-25 07:45:25 +0000

    feat: livestock dynamic breed field

--- farm_management/farm_management/doctype/livestock/livestock.js
+++ farm_management/farm_management/doctype/livestock/livestock.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, Group 2 and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Livestock", {
-// 	refresh(frm) {
+frappe.ui.form.on("Livestock", {
+	refresh(frm) {
+        frm.toggle_enable("breed", frm.doc.animal_type);
+	},
+    animal_type(frm) {
+        frm.toggle_enable("breed", frm.doc.animal_type);
+        frm.set_query("breed", () => {
+            return {
+                filters: {
+                    "animal_type": frm.doc.animal_type
+                }
+            }
+        })
 
-// 	},
-// });
+        if (frm.doc.breed) {
+            frm.set_value("breed", "")
+        }
+    }
+});
